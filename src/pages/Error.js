5cc1b30586a1b7a1016266f8288a771e0c31307c
bennetho <bennethossai@benneth.github.com>
2020-08-20 05:27:05 +0100

    ben

---
+++ src/pages/Error.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import Hero from '../components/Hero';
+// // import Banner from '../component/Banner';
+// import { Link } from 'react-router-dom';
+
+// function Error() {
+//   return (
+//     <Hero>
+//       <Link to='/contact' className='btn-primary'>
+//         return home
+//       </Link>
+//     </Hero>
+//   );
+// }
+
+// export default Error;
